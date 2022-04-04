b835b4197b47d63458d8579e206f1b8b0435cbc0
CaputHominis <19929725+CaputHominis@users.noreply.github.com>
2022-04-05 01:03:22 +0200

    feat: create equiv. FSA from regular expression

---
+++ src/main/java/de/dhbw/caput/tinf20b1/thompson/FiniteStateAutomaton.java
@@ -0,0 +1,63 @@
+package de.dhbw.caput.tinf20b1.thompson;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+final class FiniteStateAutomaton {
+	
+	public final State INITIAL_STATE;
+	public final State ACCEPTING_STATE;
+	
+	FiniteStateAutomaton( ){
+		super( );
+		INITIAL_STATE = new State( );
+		ACCEPTING_STATE = new State( );
+	}
+	
+	static FiniteStateAutomaton from( RegularExpression regularExpression ){
+		return regularExpression.generateFSA();
+	}
+	
+	boolean accepts( String string ){
+		Controller controller = instantiate( );
+		for( int i = 0; i < string.length(); ++i ){
+			controller.process( string.charAt(i) );
+		}
+		return controller.state().contains( ACCEPTING_STATE );
+	}
+	
+	private Controller instantiate( ){
+		return new Controller( );
+	}
+	
+	
+	
+	private final class Controller {
+		
+		private Set<State> states;
+		
+		private Controller( ){
+			super( );
+			states = INITIAL_STATE.resolveEpsilonTransitions( );
+		}
+		
+		private Set<State> state( ){
+			return states;
+		}
+		
+		private void process( char symbol ){
+			Set<State> nextStates = new HashSet<>( );
+			for( State state : states ){
+				nextStates.addAll( state.reactOn(symbol) );
+			}
+			Queue<State> queue = new LinkedList<>( );
+			queue.addAll( nextStates );
+			nextStates = State.resolveEpsilonTransitions( queue );
+			states = nextStates;
+		}
+		
+	}
+	
+}
---
+++ src/main/java/de/dhbw/caput/tinf20b1/thompson/State.java
@@ -0,0 +1,78 @@
+package de.dhbw.caput.tinf20b1.thompson;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+final class State {
+	
+	private Map<Character, Set<State>> transitions;
+	private List<State> epsilonTransitions;
+	
+	State( ){
+		super( );
+		transitions = new HashMap<>( );
+		epsilonTransitions = new ArrayList<>( );
+	}
+	
+	void addEpsilonTransitionTo( State target ){
+		epsilonTransitions.add( target );
+	}
+	
+	void addTransition( char literal, State target ){
+		if( null == transitions.get(literal) ){
+			transitions.put( literal, new HashSet<>() );
+		}
+		transitions.get( literal ).add( target );
+	}
+	
+	Set<State> resolveEpsilonTransitions( ){
+		return resolveEpsilonTransitions( this );
+	}
+	
+	static Set<State> resolveEpsilonTransitions( State start ){
+		Queue<State> queue = new LinkedList<>( );
+		Set<State> visited = new HashSet<>( );
+		queue.add( start );
+		visited.add( start );
+		while( !queue.isEmpty() ){
+			State state = queue.remove( );
+			for( State target : state.epsilonTransitions ){
+				if( !visited.contains(target) ){
+					queue.add( target );
+					visited.add( target );
+				}
+			}
+		}
+		return visited;
+	}
+	
+	static Set<State> resolveEpsilonTransitions( Queue<State> queue ){
+		Set<State> visited = new HashSet<>( );
+		while( !queue.isEmpty() ){
+			State state = queue.remove();
+			visited.add( state );
+			for( State target : state.epsilonTransitions ){
+				if( !visited.contains(target) ){
+					queue.add( target );
+					visited.add( target );
+				}
+			}
+		}
+		return visited;
+	}
+	
+	Set<State> reactOn( char symbol ){
+		Set<State> target = transitions.get( symbol );
+		if( null == target ){
+			return new HashSet<>( );
+		}
+		return target;
+	}
+	
+}
